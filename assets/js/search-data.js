55852aa8ff95e49ed771cbefe15347c909ba74f4
anilkagak2 <anilkagak2@gmail.com>
2026-03-22 15:57:02 +0000

    Deploying to gh-pages from @ anilkagak2/al-folio-0p6p13@def417632e41b23ab7a7113d02f8d3ac6b241b1f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-research",
+          title: "research",
+          description: "A thematic breakdown of my research along with highlighted papers.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/research/";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "",
